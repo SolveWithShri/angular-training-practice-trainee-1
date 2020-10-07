3b6b35e5b6acdbc568874a6e96b67a38bb4de8d4
Shrikant Adhav <shrikantadhav11@gmail.com>
2020-10-08 02:18:21 +0530

    Init

--- README.md
+++
@@ -1,5 +0,0 @@
-<!-- https://code.visualstudio.com/docs/typescript/typescript-compiling -->
-
-<!-- tsc -p ./tsconfig.json --watch -->
-
-<!-- node dist/user-service.js -->
--- dist/full-name-utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFullName = void 0;
-function getFullName(firstName, lastName) {
-    return firstName + " " + lastName;
-}
-exports.getFullName = getFullName;
-//# sourceMappingURL=full-name-utils.js.map
